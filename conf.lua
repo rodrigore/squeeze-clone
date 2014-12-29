f0eafdc0c7910bb552012732bbec52a6eab344c8
rodrigore <rodrigopcg@gmail.com>
2014-12-28 23:31:53 -0300

    Testing love2d for my clone of squeeze play

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(w)
+  w.title = "Squeeze-Clone"
+  w.window.width = 820--720
+  w.window.height = 480
+end
---
+++ main.lua
@@ -0,0 +1,97 @@
+local inspect = require 'lib/inspect'
+local lurker = require 'lib/lurker'
+
+function love.load()
+  love.keyboard.setKeyRepeat(true)
+
+  player = {
+    x = 192,
+    y = 240,
+    width = 24,
+    height = 24
+  }
+
+  -- generate map
+  -- map = generateMap()
+
+  map = {
+    { 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1 },
+    { 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0 },
+    { 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0 },
+    { 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
+    { 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1 },
+    { 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0 },
+    { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+    { 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0 },
+    { 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0 },
+    { 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+    { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1 },
+    { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+    { 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0 },
+    { 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+    { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0 }
+  }
+
+  -- background
+  -- love.graphics.setBackgroundColor(0, 0, 255)
+
+  -- player color
+  love.graphics.setColor(0, 255, 0)
+end
+
+function love.update(dt)
+  -- debugger (development only)
+  lurker.update()
+end
+
+function love.keypressed(key)
+  if key == "q" then love.event.quit() end
+
+  if key == "down" then
+    player.y = player.y + 24
+  elseif key == "up" then
+    player.y = player.y - 24
+  elseif key == "left" then
+    player.x = player.x - 24
+  elseif key == "right" then
+    player.x = player.x + 24
+  end
+end
+
+function love.draw()
+  -- debug
+  local statistics = ("fps: %d\n mem: %dKB\n x: %d, y: %d\n %dx%d"):format(love.timer.getFPS(), collectgarbage("count"), player.x, player.y, love.graphics.getDimensions())
+  love.graphics.printf(statistics, 620, 10, 200, 'right')
+
+  -- player
+  love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
+
+  -- bricks
+  for y =1, #map do
+    for x=1, #map[y] do
+      if map[y][x] == 1 then
+        love.graphics.rectangle("line", (x - 1) * 24, (y -1) * 24, 24, 24)
+      end
+    end
+  end
+end
+
+function generateMap()
+  map = {}
+
+  for i = 1, 20 do
+    for j = 1, 30 do
+      if (j == 1) then
+        map[i] = {}
+      end
+      map[i][j] = math.random(0, 1) * math.random(0, 1)
+    end
+  end
+
+  return map
+end
